491c1173d52be218750d0dec8d8b18a8b3b41e52
Enrique-mdos <139999854+Enrique-mdos@users.noreply.github.com>
2025-02-05 01:14:21 -0600

    primeros cambios en la api

---
+++ services/customers.service.js
@@ -0,0 +1,38 @@
+const { models } = require('./../libs/sequelize');
+const boom = require('@hapi/boom');
+
+class CustomerService {
+  constructor() {}
+
+  async create(data) {
+    const newCustomer = await models.Customer.create(data);
+    return newCustomer;
+  }
+
+  async find() {
+    const rta = await models.Customer.findAll();
+    return rta;
+  }
+
+  async findOne(id) {
+    const user = await models.Customer.findByPk(id);
+    if (!user) {
+      throw boom.notFound('user not found');
+    }
+    return user;
+  }
+
+  async update(id, changes) {
+    const customer = await this.findOne(id);
+    const rta = await customer.update(changes);
+    return rta;
+  }
+
+  async delete(id) {
+    const customer = await this.findOne(id);
+    await customer.destroy();
+    return { id };
+  }
+}
+
+module.exports = CustomerService;
