c7002a5d5ebce930d93e3bbd161afdd5475c405d
Enrique-mdos <139999854+Enrique-mdos@users.noreply.github.com>
2025-02-07 00:25:44 -0600

    relacion uno a muchos entre categories y products

--- services/category.service.js
+++ services/category.service.js
@@ -1,30 +1,41 @@
 const boom = require('@hapi/boom');
 
+const { models } = require('./../libs/sequelize');
+
 class CategoryService {
 
   constructor(){
   }
   async create(data) {
-    return data;
+    const newCategory = await models.Category.create(data);
+    return newCategory;
   }
 
   async find() {
-    return [];
+    const categories = await models.Category.findAll();
+    return categories;
   }
 
   async findOne(id) {
-    return { id };
+    const category = await models.Category.findByPk(id, {
+      include: ['products']
+    });
+    if (!category) {
+          throw boom.notFound('category not found');
+        }
+    return category;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const category = await this.findOne(id);
+    const updatedCategory = await category.update(changes);
+    return updatedCategory;
   }
 
   async delete(id) {
-    return { id };
+    const category = await this.findOne(id);
+    await category.destroy();
+    return { id }
   }
 
 }
