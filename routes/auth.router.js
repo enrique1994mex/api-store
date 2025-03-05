45a7add4621b8447c281b9d863620aa597cf6f57
Enrique-mdos <139999854+Enrique-mdos@users.noreply.github.com>
2025-03-05 14:21:38 -0600

    Implementación de recuperación de contraseña

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const passport = require('passport');
-const jwt = require('jsonwebtoken');
 
-const { config } = require('./../config/config');
+const AuthService = require('./../services/auth.service');
+const service = new AuthService();
 
 const router = express.Router();
 
@@ -11,12 +11,29 @@ router.post('/login',
   async (req, res, next) => {
   try {
     const user = req.user;
-    const payload = {
-      sub: user.id,
-      role: user.role
-    }
-    const token = jwt.sign(payload, config.jwtSecret);
-    res.json({ user, token });
+    res.json(service.signToken(user));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/recovery',
+  async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const rta = await service.resetRecovery(email);
+    res.json(rta);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/change-password',
+  async (req, res, next) => {
+  try {
+    const { token, newPassword } = req.body;
+    const rta = await service.changePassword(token, newPassword);
+    res.json(rta);
   } catch (error) {
     next(error);
   }
